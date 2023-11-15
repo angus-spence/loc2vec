fc233a86ad963d4c959a03f0c4ddb290d45acee0
angus-spence <gusmalcolmspence@gmail.com>
2023-11-15 18:04:38 +0000

    basic model implemented

--- src/loc2vec/config.py
+++ src/loc2vec/config.py
@@ -4,3 +4,6 @@ class Params(Enum):
     BATCH_SZIE = 60
     LEARNING_RATE = 0.005
     EPOCHS = 100
+    X_ANCHOR = '\\SWPUKNAS2201\dst\Data\Projects\RSIB\data\raw\loc2vec\multipolygons'
+    X_POS_ANCHOR = '\\SWPUKNAS2201\dst\Data\Projects\RSIB\data\raw\loc2vec\multipolygons'
+    X_NEG_ANCHOR = '\\SWPUKNAS2201\dst\Data\Projects\RSIB\data\raw\loc2vec\multipolygons'
