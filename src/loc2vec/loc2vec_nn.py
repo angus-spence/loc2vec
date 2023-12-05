28deb6339351c8cf88465ba7e47b0c2dc6884b13
angus-spence <gusmalcolmspence@gmail.com>
2023-12-05 18:29:35 +0000

    ResNet50 inplementation

--- src/loc2vec/loc2vec_nn.py
+++ src/loc2vec/loc2vec_nn.py
@@ -27,8 +27,7 @@ class Network(torch.nn.Module):
             nn.Conv2d(64, 32, 3, stride=1, padding=1),
             nn.MaxPool2d(2, stride=2, padding=0),
             nn.ReLU(),
-            nn.Conv2d(32, 32, 3, stride=1, padding=1),
-            nn.ReLU()
+            nn.Conv2d(32, 32, 3, stride=1, padding=1)
         )
 
     def forward(self, x):
