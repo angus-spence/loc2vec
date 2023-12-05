0d118bbf3e06e23a5e4dfc61994e7fed31340a06
angus-spence <gusmalcolmspence@gmail.com>
2023-12-05 18:25:44 +0000

    debugging loss

--- src/loc2vec/loc2vec_nn.py
+++ src/loc2vec/loc2vec_nn.py
@@ -15,17 +15,20 @@ class Network(torch.nn.Module):
         self.model = nn.Sequential(
             nn.Dropout2d(0.5),
             nn.Conv2d(in_channels, 64, 1, stride=1, padding=0),
+            nn.ReLU(),
             nn.Conv2d(64, 64, 1, stride=1, padding=0),
+            nn.ReLU(),
             nn.Conv2d(64, 128, 3, stride=1, padding=1),
             nn.MaxPool2d(2, stride=2, padding=0),
-            nn.LeakyReLU(),
+            nn.ReLU(),
             nn.Conv2d(128, 64, 3, stride=1, padding=1),
             nn.MaxPool2d(2, stride=2, padding=0),
-            nn.LeakyReLU(),
+            nn.ReLU(),
             nn.Conv2d(64, 32, 3, stride=1, padding=1),
             nn.MaxPool2d(2, stride=2, padding=0),
-            nn.LeakyReLU(),
+            nn.ReLU(),
             nn.Conv2d(32, 32, 3, stride=1, padding=1),
+            nn.ReLU()
         )
 
     def forward(self, x):
