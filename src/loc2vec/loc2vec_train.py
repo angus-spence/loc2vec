da86e76efab81122d10d052f9b11e9f1712cb723
angus-spence <gusmalcolmspence@gmail.com>
2023-12-07 14:30:36 +0000

    changes to model architecture

--- src/loc2vec/loc2vec_train.py
+++ src/loc2vec/loc2vec_train.py
@@ -2,6 +2,8 @@ from loc2vec.loc2vec_nn import Network, Loc2vec, TripletLossFunction
 from loc2vec.config import Params
 from loc2vec.data_loader import Data_Loader
 
+import csv
+
 import torch
 import numpy as np
 from tqdm import tqdm
@@ -35,36 +37,44 @@ def train(train_limit: int = None, logging: bool = True, plot: bool = False) -> 
         
         for batch in range(loader.batches):
             idx = 0
-            if train_limit and idx == train_limit:
-                break
-            else:
-                if idx < train_limit:
-                    o, plus, neg = next(loader)
-                    #if Loc2vec: 
-                    #    o = o.view(-1, *loader._image_shape())
-                    #    plus = plus.view(-1, *loader._image_shape())
-                    #    neg = neg.view(-1, *loader._image_shape())
+            while idx < train_limit:
+                o, plus, neg = next(loader)
+                #if Loc2vec: 
+                #    o = o.view(-1, *loader._image_shape())
+                #    plus = plus.view(-1, *loader._image_shape())
+                #    neg = neg.view(-1, *loader._image_shape())
 
-                    o, plus, neg = (model(o), model(plus), model(neg))
+                o, plus, neg = (model(o), model(plus), model(neg))
 
-                    loss, loss_summary, ap, an, mn = criterion(o, plus, neg)
-                    loss.backward()
-                    optimiser.step()
+                loss, loss_summary, ap, an, mn = criterion(o, plus, neg)
+                loss.backward()
+                optimiser.step()
 
-                    running_points.append([o.cpu().detach().numpy(), plus.cpu().detach().numpy(), neg.cpu().detach().numpy()])
-                    running_loss.append(loss.cpu().detach().numpy())
-                    ap_log.append(ap)
-                    an_log.append(an)
-                    mn_log.append(mn)
+                running_points.append([o.cpu().detach().numpy(), plus.cpu().detach().numpy(), neg.cpu().detach().numpy()])
+                running_loss.append(loss.cpu().detach().numpy())
+                ap_log.append(ap)
+                an_log.append(an)
+                mn_log.append(mn)
 
-                    del o, plus, neg
+                del o, plus, neg
 
-                    print(f'Batch: {batch+1}/{loader.batches} - Running Loss: {round(float(np.mean(running_loss)), 3)}')
-                    print(loss_summary)
-
-                    idx += 1
+                print(f'Batch: {batch+1}/{loader.batches} - Running Loss: {round(float(np.mean(running_loss)), 3)}')
+                print(loss_summary)
+                idx += 1
+            break
 
     torch.save(model, "loc2vec_model")
+
+    if logging:
+        import pandas as pd
+
+        df = pd.DataFrame({'running_loss': running_loss,
+                           'point_o': [running_points[i][0] for i in len(running_points)],
+                           'point_+': [running_points[i][1] for i in len(running_points)],
+                           'point_-': [running_points[i][2] for i in len(running_points)],
+                           'distance_ap': ap_log,
+                           'distance_neg': an_log,
+                           'distance_mn': mn_log}).to_csv('loc2vec_log')
 
     if plot:
         import matplotlib.pyplot as plt
@@ -83,7 +93,7 @@ def train(train_limit: int = None, logging: bool = True, plot: bool = False) -> 
         ax[0, 2].plot(iters, mn_log)
         ax[0, 2].set_title("EUCLIDEAN DISTANCE BETWEEN TRIPLETS")
 
-        plt.savefig("Loc2vec_plot")
+        plt.savefig("Loc2vec_plot", dpi=150)
 
 if __name__ == "__main__":
     train(plot=True, train_limit=25)
