e72f73b710c4368ea76711a16a1043b6e0edc3d9
angus-spence <gusmalcolmspence@gmail.com>
2023-12-17 21:11:20 +0000

    triplet miner implementation =

--- src/loc2vec/triplet_mining.py
+++ src/loc2vec/triplet_mining.py
@@ -1,10 +1,16 @@
+from typing import Any
 from loc2vec.loc2vec_nn import Network
 from loc2vec.data_loader import Data_Loader
 from loc2vec.config import Params
 
 from dataclasses import dataclass
+from itertools import chain
 
+import os
 import torch
+import torch.nn.functional as F
+import torchvision as tv
+from tqdm import tqdm
 
 @dataclass
 class TripletMiner:
@@ -25,24 +31,77 @@ class TripletMiner:
     difficulty: float
         difficulty of hard triplets
     """
+    image_dir: str
     model: torch.nn.Module
     device: str    
     weights: str
     sh_ratio: float
 
     def __post_init__(self):
-        self.loader = Data_Loader(x_path=Params.X_PATH.value, x_pos_path=Params.X_POS_PATH.value, batch_size=2)
         self.model.load_state_dict(torch.load('src/loc2vec/loc2vec_model', map_location=torch.device(self.device)))
 
+
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        return self._evaluate_embeddings()
+
+    def _strp_ids(self, path: str, splitter: str, pos: int):
+        """
+        Extract list of file IDs from file name
+
+        Parameters
+        ----------
+        path: str
+            path of intput files
+        splitter: str
+            delineator in file name exposing id
+        pos: int
+            split index to use as id
+
+        Returns
+        -------
+        ids: list
+            list of identifiers
+        """
+        ids = []
+        for root, dir, file in os.walk(path):
+            if file and splitter: ids.append(str(file).strip(splitter)[pos])
+            elif file: ids.append(file) 
+        return ids
+
+    def _path_stack(self):
+        paths, stacker, channels = [], [], []
+        for root, dir, file in os.walk(self.image_dir):
+            if file: paths.append(file)
+            if dir: channels.append(dir)
+        channels = list(chain.from_iterable(channels))
+        print(len(paths))
+        for i in tqdm(range(len(paths))):
+            stacker.append([os.path.join(self.image_dir, channels[j], paths[j][i]) for j in range(len(channels))])
+        print(stacker)
+        return stacker
+
     def _evaluate_embeddings(self):
-        
-        for img in range(len(self.loader)):
-            return
+        embs = []
+        for channel in self._path_stack():
+            img_tensor = []
+            img_tensor.append([tv.io.read_image(img)[:3,:,:].type(torch.float).to(self.device) for img in channel])
+            img_tensor = list(chain.from_iterable(img_tensor))
+            tensor_out = torch.cat(img_tensor)
+            print(tensor_out.shape)
+            embs.append(self.model(tensor_out))
+        return embs
+    
+    def _pairwise_distance_matrix(self):
+        pwdm = []
+        print(self.embs)
+        for img_x in self.embs:
+            pwdm = torch.stack(pwdm.append([F.pairwise_distance(img_x, i) for i in img_x]))
+        return pwdm
 
 if __name__ == "__main__":
-
-    miner = TripletMiner(model=Network(in_channels=15), 
+    miner = TripletMiner(image_dir=r'C:\Users\aspence1\Documents\loc2vec_data\x_pos',
+                         model=Network(in_channels=15), 
                          device='cpu', 
                          weights='src/loc2vec/loc2vec_model', 
                          sh_ratio=0.8)
-    miner._evaluate_embeddings()
+    print(len(miner._evaluate_embeddings()))
